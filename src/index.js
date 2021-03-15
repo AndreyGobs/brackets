df3edf4cad9056cd9b2da7a8ca7c60d441c2b3f4
AndreyGobs <55401672+AndreyGobs@users.noreply.github.com>
2021-03-15 23:17:18 +0300

    feat: add check func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str.split('');
+  let start = [];
+  let end = [];
+  let template = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      for (let k = 0; k < 1; k++){
+          start.push(bracketsConfig[i][k]);
+          end.push(bracketsConfig[i][k+1]);
+      }
+  }
+
+  for (let i = 0; i < str.length; i++) {
+      startIndex = start.indexOf(str[i]);
+      if (startIndex !== -1) {
+          template.push(startIndex);
+          continue;
+      }
+
+      endIndex = end.indexOf(str[i]);
+      if (endIndex !== -1) {
+          startIndex = template.pop();
+          if (endIndex !== startIndex) {
+              return false;
+          }
+      }
+  }
+
+  if (template.length !== 0) {
+      return false;
+  }
+
+  return true;
 }
